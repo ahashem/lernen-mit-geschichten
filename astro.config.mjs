4d2e23fe62f0a60dea8dc3750af653c7b32e8bcf
Ahmed Abdelrahman <ahashem.pm@gmail.com>
2025-10-05 19:06:13 +0200

    feat: prepare gh-pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
-import keystatic from '@keystatic/astro';
 import partytown from '@astrojs/partytown';
 
 export default defineConfig({
   integrations: [
     mdx(),
     react(),
-    keystatic(),
     partytown({
       config: {
         forward: ['dataLayer.push'],
@@ -16,7 +14,8 @@ export default defineConfig({
     }),
   ],
   output: 'static',
-  site: 'https://lernen-mit-geschichten.de',
+  site: 'https://ahashem.github.io',
+  base: '/lernen-mit-geschichten',
   i18n: {
     defaultLocale: 'de',
     locales: ['de', 'ar', 'en', 'tr', 'ur'],
